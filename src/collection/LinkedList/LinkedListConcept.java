2270803d43b4f7a2032aa7584a6f1518a7b46bc7
Mayuryadav123 <yadavmayur2597@gmail.com>
2021-11-26 12:09:46 +0530

    Collection-LinkList

---
+++ src/collection/LinkedList/LinkedListConcept.java
@@ -0,0 +1,29 @@
+package collection.LinkedList;
+
+
+
+import java.util.LinkedList;
+
+public class LinkedListConcept {
+    public static void main(String[] args) {
+        LinkedList<String >L1=new LinkedList<String >();
+        L1.add("JAVA");
+        L1.add("JAVASCRIPT");
+        L1.add("CSS");
+        L1.add("C++");
+        L1.add("PYTHON");
+        System.out.println(L1);
+
+
+        //add
+        L1.addFirst("Spring");
+        L1.addLast("Spring boot");
+        System.out.println(L1);
+
+        //get set
+        System.out.println(L1.get(2));
+        System.out.println(L1.set(3,"HTML"));
+        System.out.println(L1);
+
+    }
+}
