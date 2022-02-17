3f3be26d33207231ea54dc7e265bf89220ec7605
Mayuryadav123 <yadavmayur2597@gmail.com>
2022-02-17 07:51:32 +0530

    Java Thread Creation

--- src/Multithreading/Practice/ThreadCreation.java
+++ src/Multithreading/Practice/ThreadCreation.java
@@ -1,13 +1,15 @@
 package Multithreading.Practice;
 
 public class ThreadCreation extends Thread{
-    public void run(){
+    public void run() {
         System.out.println("Thread creation by thread class");
     }
+
 
     public static void main(String[] args) throws InterruptedException {
         ThreadCreation obj=new ThreadCreation();
         obj.start();
-        Thread.sleep(100);
+        Thread.sleep(500);
     }
+
 }
